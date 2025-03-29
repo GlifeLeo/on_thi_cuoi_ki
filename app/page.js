0ec6ca4788ac8246a466933799b5b509d4f9946a
Leo <leo.pham@glife.com.sg>
2025-03-29 10:29:13 +0700

    timer g

--- app/page.js
+++ app/page.js
@@ -1,103 +1,54 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [seconds, setSeconds] = useState(3800)
+  const [isRunning, setRunning] = useState(false)
+  const [history, setHistory] = useState([])
+  const aa = [{ sc: "00:00", saveTime: "dd/mm/yyyy 00:00 am" }]
+  useEffect(() => {
+    if (isRunning == true)
+      setTimeout(() => {
+        setSeconds(seconds + 1)
+      }, 1000); // milliseconds
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  }, [seconds, isRunning])
+
+  function handleStart() {
+    setSeconds(seconds + 1)
+    setRunning(true)
+  }
+
+  function handlePause() {
+    setRunning(false)
+  }
+  const hours = Math.floor(seconds / 3600)
+  const minutes = Math.floor((seconds % 3600) / 60)
+  const showSecond = seconds % 60
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="p-4 w-full bg-black text-white">Menu</div>
+      <div className="text-7xl py-10 font-bold">
+        {hours ? (hours < 10 ? `0${hours}` : hours) + ":" : ""}
+        {hours || minutes ? (hours < 10 ? `0${minutes}` : minutes) + ":" : ""}
+        {showSecond < 10 ? `0${showSecond}` : showSecond}
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <button onClick={handleStart} className="rounded-lg px-10 py-2 bg-red-500">Start</button>
+        <button onClick={handlePause} className="rounded-lg px-10 py-2 bg-blue-500">Pause</button>
+        <button className="rounded-lg px-10 py-2 bg-green-500">Reset</button>
+        <button className="rounded-lg px-10 py-2 bg-pink-500">Save</button>
+      </div>
+      <div className="mt-10 h-40 bg-yellow-50 w-3/5 p-6">
+        History
+        {aa.map((his, index) => {
+          return <div key={index} className="flex gap-3">
+            <span> {his.sc}</span>
+            <span>{his.saveTime}</span>
+          </div>
+        })}
+      </div>
     </div>
   );
 }
